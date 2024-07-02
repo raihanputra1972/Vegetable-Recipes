cd1c874c28c98236aa6f58b7622f051c01edc561
raihanputra1972 <raihankyla1305@gmail.com>
2024-07-02 20:13:18 +0700

    update contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -28,9 +28,9 @@ function Contact() {
     <div>
       <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="to_name" />
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="from_email" />
         <label>Message</label>
         <textarea name="message" />
         <input type="submit" value="Send" />
